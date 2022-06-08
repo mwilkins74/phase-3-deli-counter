0d8138036a3bf13c17c80adaafb5c2de3ec7dedc
Mark <markwilkins86@gmail.com>
2022-06-08 13:43:21 -0400

    commit tree 5a98f731b26f108ef118701f919634815152b2e6

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def line(deli)
+    if deli.empty?
+        puts "The line is currently empty."
+    else 
+        current_line = "The line is currently:"
+        deli.each.with_index(1) do |name, index|
+        current_line << " #{index}. #{name}"
+       end
+       puts current_line
+    end
+end
+
+def take_a_number(deli, name)
+    deli << name
+    puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
